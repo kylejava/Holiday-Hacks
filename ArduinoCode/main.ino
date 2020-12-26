b9bdd50441a2af5ebb052fc926186a64caad1cbf
Neala Mendoza <mendo151@cougars.csusm.edu>
2020-12-25 21:12:36 -0800

    Added starter code

--- ArduinoCode/main.ino
+++ ArduinoCode/main.ino
@@ -1,9 +1,23 @@
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5};
+int duration = 500;  // 500 miliseconds
+
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+    // pin8 output the voice, every scale is 0.5 sencond
+    tone(8, melody[thisNote], duration);
+
+    // Output the voice after several minutes
+    delay(1000);
+  }
+
+  // restart after two seconds
+  delay(2000);
 }
