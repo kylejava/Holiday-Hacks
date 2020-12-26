d42e11a6826d675b4fade4d201c6eb2e8b8ce271
kylejava <kyleedwardgjava@gmail.com>
2020-12-26 01:21:40 -0800

    Initialized the notes

--- ArduinoCode/main.ino
+++ ArduinoCode/main.ino
@@ -1,13 +1,42 @@
 #include "pitches.h"
+//Defining pins
+int c_note  = 6;
+int d_note  = 7;
+int e_note = 8;
+int f_note 9
+int g_note =  10;
+int a_note =  11;
+int b_note =  12;
+int c2_note =13;
+int buzzerPin = 5;
 
 //NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6
 // notes in the melody:
 int melody[] = {
-  NOTE_C6, NOTE_B5, NOTE_A5, NOTE_G5, NOTE_F5, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_G5, NOTE_A5, NOTE_A5, NOTE_B5, NOTE_B5, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_B5, NOTE_A5, NOTE_G5, NOTE_G5, NOTE_F5, NOTE_E5, NOTE_C6, NOTE_C6, NOTE_B5, NOTE_A5, NOTE_G5, NOTE_G5, NOTE_F5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_F5, NOTE_E5, NOTE_D5, NOTE_D5, NOTE_D5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_E5, NOTE_D5, NOTE_C6, NOTE_C6, NOTE_A5, NOTE_G5, NOTE_F5, NOTE_E5, NOTE_F5, NOTE_E5, NOTE_D5, NOTE_C5};
+  NOTE_C6, NOTE_B5, NOTE_A5, NOTE_G5, NOTE_F5,
+   NOTE_E5, NOTE_D5, NOTE_C5, NOTE_G5, NOTE_A5,
+   NOTE_A5, NOTE_B5, NOTE_B5, NOTE_C6, NOTE_C6,
+   NOTE_C6, NOTE_B5, NOTE_A5, NOTE_G5, NOTE_G5,
+   NOTE_F5, NOTE_E5, NOTE_C6, NOTE_C6, NOTE_B5,
+   NOTE_A5, NOTE_G5, NOTE_G5, NOTE_F5, NOTE_E5,
+   NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_F5,
+   NOTE_G5, NOTE_F5, NOTE_E5, NOTE_D5, NOTE_D5,
+   NOTE_D5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_E5,
+   NOTE_D5, NOTE_C6, NOTE_C6, NOTE_A5, NOTE_G5,
+   NOTE_F5, NOTE_E5, NOTE_F5, NOTE_E5, NOTE_D5,
+   NOTE_C5};
 int duration = 500;  // 500 miliseconds
 
 void setup() {
-
+  pinMode(c_note , OUTPUT);
+  pinMode(d_note , OUTPUT);
+  pinMode(e_note , OUTPUT);
+  pinMode(f_note , OUTPUT);
+  pinMode(g_note , OUTPUT);
+  pinMode(a_note , OUTPUT);
+  pinMode(b_note , OUTPUT);
+  pinMode(c2_note , OUTPUT);
+  pinMode(buzzerPin , OUTPUT);
 }
 
 void loop() {
